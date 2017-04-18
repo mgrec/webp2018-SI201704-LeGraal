7e8b49ae3b9edb27caebc6cee2736c180d878b61
GoyonStephane <stephane.goyon@gmail.com>
2017-04-18 17:47:08 +0200

    Skeletor

---
+++ app/assets/js/scroll.js
@@ -0,0 +1,25 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2017. MIT licensed.
+ */
+/**
+ * Created by StephaneGoyon on 18/04/2017.
+ */
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
