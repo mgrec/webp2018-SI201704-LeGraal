9e6554528d5ff728c4ef1ed3b6b4fb391e35b150
Grec Maxime <maxime.grec@gmail.com>
2017-04-19 16:00:31 +0200

    add templates

--- Model/adminRepository.php
+++ Model/adminRepository.php
@@ -11,5 +11,27 @@ namespace Model;
 
 class adminRepository
 {
+    public function connectionAction($array, $pdo){
+        $query = "SELECT id FROM administrators WHERE email = :email AND password = :password";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindValue(':email', $array['email']);
+        $stmt->bindValue(':password', $array['password']);
+        $stmt->execute();
+        $res = $stmt->fetch();
 
+        if ($res != null){
+            $arrayRtn  = [];
+            $arrayRtn['id'] = $res['id'];
+            $arrayRtn['code'] = true;
+            $arrayRtn['email'] = $array['email'];
+
+            return $arrayRtn;
+        }else{
+            $arrayRtn  = [];
+            $arrayRtn['code'] = false;
+            $arrayRtn['email'] = $array['email'];
+
+            return $arrayRtn;
+        }
+    }
 }
