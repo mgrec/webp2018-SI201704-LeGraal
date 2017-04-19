631831eeb7434d1e3863f6e7a8577d3ad5410092
quengeorges <quenting92@gmail.com>
2017-04-19 10:54:55 +0200

    test pdo

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 require 'vendor/autoload.php';
 require_once "config.php";
 use Controller\indexController;
+use Controller\adminController;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -30,5 +31,15 @@ $app->post('/contact/', function(Request $request){
 $app->get('/admin', function($request, $response, $arg) {
     return $this->view->render($response, 'admin/connection.twig');
 })->setName('adminConnection');
+
+$app->get('/connect', function(Request $request, $response, $args) {
+    $dbhandler = $this->db;
+    $queryUsers = $dbhandler->prepare("SELECT * FROM user");
+    $queryUsers->execute();
+    $users = $queryUsers->fetchAll();
+    var_dump($users);
+    die();
+
+});
 
 $app->run();
